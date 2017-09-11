862fd1ec13a8761bde7721356b79235c798f4beb
Yonadav Shavit <yonadav@mit.edu>
2017-09-11 16:37:10 -0400

    More modifications to setup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 import os
-from scipy.misc import imsave
 from input_data import read_data_sets
 
 setup(name='gym_mnist',
       version='0.0.2',
-      install_requires=['certifi','gym','pillow']
+      install_requires=['numpy','certifi','gym','pillow','opencv-python']
 )
+import cv2
 this_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = this_dir + "/gym_mnist/resources/"
 zip_dir = data_dir + "zipfiles/"
@@ -22,9 +22,9 @@ images = data_sets.train.images
 labels = data_sets.train.labels
 c = [0]*10
 print ("Saving assorted MNIST digits to disk...")
+print(images.shape)
 for i in range(len(images)):
     l = labels[i]
     im = images[i, :].reshape((28,28))
-    imsave(data_dir + "digit" + str(l) + "/im" + str(c[l]) + ".png", im)
+    cv2.imwrite(data_dir + "digit" + str(l) + "/im" + str(c[l]) + ".png", im)
     c[l] += 1
-
